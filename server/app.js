0b35485808712b3e649c2e540f9af2757323cc4e
Vincent Li (RIT Student) <vl5604@rit.edu>
2022-10-12 13:46:35 -0400

    10/12 inclass

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
 
 const router = require('./router.js');
 
@@ -16,6 +17,21 @@ app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// app.set sets one of the express config options
+// set up the view (V of MVC) to use handlebars
+// You can use other view engines besides handlebars
+// Here we are configuring handlebars as the engine by passing in the engine
+// function from the library. We also set the default layout to nothing.
+app.engine('handlebars', expressHandlebars.engine({
+  defaultLayout: '',
+}));
+app.set('view engine', 'handlebars');
+
+
+// set the views path to the template directory
+// (not shown in this example but needed for express to work)
+app.set('views', `${__dirname}/../views`);
 
 router(app);
 
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,29 +1,49 @@
 // There is no reason for the name here except as an
 // example of how to set something for the POST
 let name = 'unknown';
+let names = ['bobby','rob','tommy','tim']
 
 const hostIndex = (req, res) => {
-
+  res.render('index', {
+    title: 'home',
+    pageName: 'Home Page',
+    name: name,
+  })
 };
 
 const hostPage1 = (req, res) => {
+  const filtered = names.filter(x => x.length <= 4);
 
+  res.render('page1', {
+    title: 'page one',
+    pageName: 'Home Page',
+    name: name,
+    names: filtered,
+  })
 };
 
 const hostPage2 = (req, res) => {
-
+  res.render('page2')
 };
 
 const getName = (req, res) => {
-
+  res.json(name);
 };
 
 const setName = (req, res) => {
+  if (!req.body.firstname || !req.body.lastname) {
+    return res.status(400).json({
+      error: 'first name and last name are both required',
+      id: 'setNameMissingParams',
+    })
+  }
 
+  name = req.body.firstname + " " + req.body.lastname;
+  return res.status(200).json(name);
 };
 
 const notFound = (req, res) => {
-
+  res.status(404).render('notFound', { page: req.url });
 };
 
 module.exports = {
